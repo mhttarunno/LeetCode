072376cee226bfa57db4a20d5b0efb84519c58bd
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-07-13 23:27:31 +0600

    Time: 0 ms (100.00%) | Memory: 23.7 MB (80.06%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                cnt++;
+            } else {
+                nums[i - cnt] = nums[i];
+            }
+        }
+        for (int i = n - cnt; i < n; ++i) {
+            nums[i] = 0;
+        }
+    }
+};
+
