5442250790924eb6f9f7f3ed06c3703047c615b4
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-07-15 12:57:38 +0600

    Time: 0 ms (100.00%) | Memory: 31.4 MB (9.55%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int low, int high, int target) {
+        int n = nums.size();
+        if(low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if(nums[mid] == target) {
+            return mid;
+        } 
+        else if(target > nums[mid]) {
+            return binarySearch(nums, mid + 1, high, target);
+        }
+        return binarySearch(nums, low, high - 1, target);
+    }
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
+    }
+};
