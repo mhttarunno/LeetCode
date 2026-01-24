37bd7058df1d9453eb6e97b2f159c67fd2257ca8
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-01-24 13:17:12 +0600

    Time: 1 ms (55.44%) | Memory: 9.8 MB (25.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string ss) {
-        sort(s.begin(), s.end());
-        sort(ss.begin(), ss.end());
-        if (s == ss) {
-            return true;
-        } else {
+        if (s.size() ^ ss.size()) {
             return false;
         }
+        unordered_map<int, int> mp;
+        for (int i = 0; i < s.size(); ++i) {
+            mp[s[i]]++;
+        }
+        for (int i = 0; i < ss.size(); ++i) {
+            if (mp.find(ss[i]) != mp.end()) {
+                mp[ss[i]]--;
+            } else {
+                return false;
+            }
+        }
+        for (auto& it : mp) {
+            if (it.second != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
