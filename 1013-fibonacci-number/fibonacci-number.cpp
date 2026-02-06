7d4e11a7346428569b8a7a6d0fe041a9a956e743
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-02-07 01:15:17 +0600

    Time: 3 ms (64.70%) | Memory: 8.1 MB (18.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fib(n - 1) + fib(n - 2);
+        vector<int> ans = {0, 1};
+        for (int i = 2; i <= n; ++i) {
+            ans.push_back(ans[i - 1] + ans[i - 2]);
+        }
+        return ans[n];
     }
 };
