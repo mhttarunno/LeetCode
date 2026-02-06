e04c24288bbb0c030a73931043ef3381e69503d4
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-02-07 00:47:39 +0600

    Time: 1 ms (43.32%) | Memory: 11 MB (23.74%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> ans;
+    vector<int> ds;
+
+    void printF(vector<int>& v, vector<int>& freq) {
+        if (ds.size() == v.size()) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for (int i = 0; i < v.size(); ++i) {
+            if (freq[i] == false) {
+                ds.push_back(v[i]);
+                freq[i] = true;
+                printF(v, freq);
+                freq[i] = false;
+                ds.pop_back();
+            }
+        }
+    }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        do {
-            ans.push_back(nums);
-        } while (next_permutation(nums.begin(), nums.end()));
+        ans.clear();
+        ds.clear();
+        vector<int> freq(nums.size(), 0);
+        printF(nums, freq);
         return ans;
     }
 };
