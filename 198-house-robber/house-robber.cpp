6fd674f59ab511b559534a76d28337e4d5683f4d
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-02-07 20:29:31 +0600

    Time: 0 ms (100.00%) | Memory: 10.5 MB (62.15%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
+    // Tabulation:
     vector<int> dp;
     int printF(int idx, vector<int>& v) {
-        if (idx == 0) return v[idx];
-        if (idx < 0) return 0;
-        if (dp[idx] != -1) return dp[idx];
-        int pick = v[idx] + printF(idx - 2, v);
-        int nonPick = printF(idx - 1, v);
-        return dp[idx] = max(pick, nonPick);
+        dp[0] = 0;
+        for (int i = 1; i <= v.size(); ++i) {
+            int pick = v[i - 1];
+            if(i > 1) {
+                pick += dp[i - 2];
+            }
+            int nonPick = 0 + dp[i - 1];
+            dp[i] = max(pick, nonPick);
+        }
+        return dp[v.size()];
     }
     int rob(vector<int>& nums) {
         dp.resize(nums.size() + 1, -1);
