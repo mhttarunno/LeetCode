8d4d5e9e063f7b1e15406c017becfa45770a4e04
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-02-07 22:07:50 +0600

    Time: 0 ms (100.00%) | Memory: 11.1 MB (7.64%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> dp;
-    int printF(int idx, vector<int>& v) {
-        if (idx == 0)
-            return v[idx];
-        if (idx < 0)
-            return 0;
-        if (dp[idx] != -1)
-            return dp[idx];
-        int pick = v[idx] + printF(idx - 2, v);
-        int notPick = printF(idx - 1, v);
-        return dp[idx] = max(pick, notPick);
+    int printF(vector<int>& v) {
+        dp[0] = v[0];
+        for (int i = 1; i < v.size(); ++i) {
+            int pick = v[i];
+            if (i > 1) pick += dp[i - 2];
+            int notPick = dp[i - 1];
+            dp[i] = max(pick, notPick);
+        }
+        return dp[v.size() - 1];
     }
+
     int rob(vector<int>& nums) {
         if (nums.size() == 1)
             return nums[0];
@@ -19,11 +19,11 @@ public:
         vector<int> tmp1(nums.begin() + 1, nums.end());
         vector<int> tmp2(nums.begin(), nums.end() - 1);
 
-        dp.assign(tmp1.size() + 1, -1);
-        int op1 = printF(tmp1.size() - 1, tmp1);
+        dp.resize(tmp1.size());
+        int op1 = printF(tmp1);
 
-        dp.assign(tmp2.size() + 1, -1);
-        int op2 = printF(tmp2.size() - 1, tmp2);
+        dp.resize(tmp2.size());
+        int op2 = printF(tmp2);
 
         return max(op1, op2);
     }
