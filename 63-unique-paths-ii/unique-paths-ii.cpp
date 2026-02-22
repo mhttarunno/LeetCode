1bc013708ab6500621517f86fc71c716df72bfd4
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-02-22 14:34:41 +0600

    Time: 2 ms (13.35%) | Memory: 12 MB (30.19%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int gridDestination(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || j < 0) return 0;
-        if (grid[i][j] == 1) return 0;
-        if (i == 0 && j == 0) return 1;
-        if (dp[i][j] != -1) return dp[i][j];
-        int up = gridDestination(i - 1, j, grid);
-        int left = gridDestination(i, j - 1, grid);
-        return dp[i][j] = up + left;
+    int gridDestination(int m, int n, vector<vector<int>>& grid) {
+        dp[0][0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) dp[i][j] = 0;
+                else if (i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if (i > 0) up = dp[i - 1][j];
+                    if (j > 0) left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        dp.assign(m, vector<int>(n, -1)); 
-        return gridDestination(m - 1, n - 1, obstacleGrid);
+        dp.assign(m, vector<int>(n, 0));
+        return gridDestination(m, n, obstacleGrid);
     }
 };
