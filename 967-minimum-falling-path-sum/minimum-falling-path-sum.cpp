8c71994baef5ed15718e7ccb3b0afa74f1f6638c
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-02-28 02:58:45 +0600

    Time: 2 ms (40.21%) | Memory: 15.1 MB (42.75%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> dp = grid;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int up = dp[i - 1][j];
+                int leftDiag = (j > 0) ? dp[i - 1][j - 1] : INT_MAX;
+                int rightDiag = (j < n - 1) ? dp[i - 1][j + 1] : INT_MAX;
+                dp[i][j] += min({up, leftDiag, rightDiag});
+            }
+        }
+        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
+    }
+};
