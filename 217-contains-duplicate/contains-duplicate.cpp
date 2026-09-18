c9c8f74b89de21e8dc8a5488ac41f8f8d166d442
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2026-09-18 11:01:38 +0600

    Time: 135 ms (10.32%) | Memory: 116.2 MB (5.04%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-        return s.size() != nums.size();
+        return set<int> (nums.begin(), nums.end()).size() != nums.size();
     }
 };
